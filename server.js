41c94242f339ca90333d6876018b00c709f55eeb
Orlando Macedo <orlando.macedo15@ua.pt>
2021-01-18 12:03:07 +0000

    alterar server.js

--- server.js
+++ server.js
@@ -2,14 +2,16 @@
 const express = require('express');
 const path = require('path');
 
+const nomeApp = process.env.npm_package_name;
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 // Start the app by listening on the default Heroku port
